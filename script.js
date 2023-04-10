2579b93872ed18ba51d6902362f256728e3077eb
painter98 <120320713+painter98@users.noreply.github.com>
2023-04-10 18:04:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // complete the given function
 
 function palindrome(str){
+	str = str.toLowerCase();
+	
 	for(let i=0;i<str.length;i++){
 		if(str.charAt(i)!==str.charAt(str.length-i-1)) return false;
 	}
